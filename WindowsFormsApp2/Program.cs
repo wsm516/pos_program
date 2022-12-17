38b153dccbe26e286aa8c62780697c5adca10c72
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-17 19:59:59 +0900

    Update Program.cs

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApp2
 {
     internal static class Program
     {
-        /// <summary> 
+        /// <summary>  
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
