7510247d99587ceffcd3f68b411af0f504baf0e4
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-17 19:59:54 +0900

    Update Program.cs

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+ 
 namespace WindowsFormsApp2
 {
     internal static class Program
