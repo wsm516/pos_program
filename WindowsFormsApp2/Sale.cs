21bb002c89411a1d9de38ccc12f4beb5a7bf369c
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-17 19:59:08 +0900

    Update Sale.cs

--- WindowsFormsApp2/Sale.cs
+++ WindowsFormsApp2/Sale.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
+ 
 namespace WindowsFormsApp2
 {
     public partial class Sale : Form
